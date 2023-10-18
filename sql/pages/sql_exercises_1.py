5e3dc2eccf66cda4ab3c8c27906a25466a28c5c3
py-miracl6 <122025075+py-miracl6@users.noreply.github.com>
2023-10-18 19:57:38 +0300

    Update sql_exercises_1.py

--- sql/pages/sql_exercises_1.py
+++ sql/pages/sql_exercises_1.py
@@ -2,46 +2,9 @@ import streamlit as st
 from streamlit_ace import st_ace
 from sqlite3 import connect
 import pandas as pd
-from sql_func import show_tables, hide_part_of_page, check_update_db
+from sql_func import show_tables, hide_part_of_page, check_update_db, init_logging
 import logging
-from streamlit import runtime
-from streamlit.runtime.scriptrunner import get_script_run_ctx
 
-
-def get_remote_ip() -> str:
-    """Get remote ip."""
-    try:
-        ctx = get_script_run_ctx()
-        if ctx is None:
-            return None
-        session_info = runtime.get_instance().get_client(ctx.session_id)
-        if session_info is None:
-            return None
-    except Exception as e:
-        return None
-    return session_info.request.remote_ip
-
-
-class ContextFilter(logging.Filter):
-    def filter(self, record):
-        record.user_ip = get_remote_ip()
-        return super().filter(record)
-
-
-def init_logging():
-    # create a custom logger
-    logger = logging.getLogger("foobar")
-    if logger.handlers:  # logger is already setup, don't setup again
-        return
-    logger.propagate = False
-    logger.setLevel(logging.INFO)
-    # in the formatter, use the variable "user_ip"
-    formatter = logging.Formatter("%(name)s %(asctime)s %(levelname)s [user_ip=%(user_ip)s] - %(message)s")
-    handler = logging.StreamHandler()
-    handler.setLevel(logging.INFO)
-    handler.addFilter(ContextFilter())
-    handler.setFormatter(formatter)
-    logger.addHandler(handler)
 
 hide_part_of_page()
 
