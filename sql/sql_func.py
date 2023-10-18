61a6f69e989ea7772d633e161758302ae225593f
py-miracl6 <122025075+py-miracl6@users.noreply.github.com>
2023-10-18 19:57:02 +0300

    Update sql_func.py

--- sql/sql_func.py
+++ sql/sql_func.py
@@ -1,6 +1,9 @@
 import streamlit as st
 import pandas as pd
 from sqlite3 import connect
+import logging
+from streamlit import runtime
+from streamlit.runtime.scriptrunner import get_script_run_ctx
 
 
 def show_tables(url_db: str = "data/EmployeeSQL.db"):
@@ -57,3 +60,44 @@ def check_update_db(content):
     assert (
         "update" not in content.lower()
     ), "В данном функционале не предусмотрено изменение и создание таблиц"
+
+
+def get_remote_ip() -> str:
+    """Get remote ip."""
+
+    try:
+        ctx = get_script_run_ctx()
+        if ctx is None:
+            return None
+
+        session_info = runtime.get_instance().get_client(ctx.session_id)
+        if session_info is None:
+            return None
+    except Exception as e:
+        return None
+
+    return session_info.request.remote_ip
+
+class ContextFilter(logging.Filter):
+    def filter(self, record):
+        record.user_ip = get_remote_ip()
+        return super().filter(record)
+
+def init_logging():
+    # Make sure to instanciate the logger only once
+    # otherwise, it will create a StreamHandler at every run
+    # and duplicate the messages
+
+    # create a custom logger
+    logger = logging.getLogger("foobar")
+    if logger.handlers:  # logger is already setup, don't setup again
+        return
+    logger.propagate = False
+    logger.setLevel(logging.INFO)
+    # in the formatter, use the variable "user_ip"
+    formatter = logging.Formatter("%(name)s %(asctime)s %(levelname)s [user_ip=%(user_ip)s] - %(message)s")
+    handler = logging.StreamHandler()
+    handler.setLevel(logging.INFO)
+    handler.addFilter(ContextFilter())
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
