3ecf703b5f6871385de25af7773b9bbde390782a
py-miracl6 <122025075+py-miracl6@users.noreply.github.com>
2024-02-11 16:05:55 +0300

    Update sql_exercises_1.py

--- sql/pages/sql_exercises_1.py
+++ sql/pages/sql_exercises_1.py
@@ -29,30 +29,38 @@ content = st_ace(
     key="ace",
 )
 
-if content:
-    init_logging()
-    conn = connect("data/EmployeeSQL.db")
-    st.markdown("### Результат")
-    test_sql = """select * from dept_emp LIMIT 10"""
-    logger = logging.getLogger("foobar")
-    try:
-        check_update_db(content=content)
-        logger.info(f"Start write query: {content}")
-        df = pd.read_sql(content, conn)[:80]
-        st.dataframe(df)
-        df_check = pd.read_sql(test_sql, conn)
-        assert (
-            len(set(df.columns) ^ set(df_check.columns)) == 0
-        ), "Проверьте название таблицы"
-        assert (
-            df.shape[0] == df_check.shape[0]
-        ), "Проверьте размер таблицы, получаемый в ходе выполнения скрипта"
-        assert df_check.equals(df), "Проверьте, что скрипт написан согласно заданию"
-        st.success("Все верно! Ключ = 131")
-    except Exception as ex:
-        if ("Проверьте" in str(ex)) or ("не предусмотрено" in str(ex)):
-            st.error(ex)
-        else:
-            st.error(
-                f"Скрипт написан некорретно (неполностью, либо вовсе отсутствует). Error message: {ex}"
-            )
+try:
+    if content:
+        init_logging()
+        conn = connect("../data/EmployeeSQL.db")
+        st.markdown("### Результат")
+        test_sql = """select * from dept_emp LIMIT 10"""
+        logger = logging.getLogger("foobar")
+        try:
+            check_update_db(content=content)
+            logger.info(f"Start write query: {content}")
+            df = pd.read_sql(content, conn)[:80]
+            st.dataframe(df)
+            df_check = pd.read_sql(test_sql, conn)
+            assert (
+                len(set(df.columns) ^ set(df_check.columns)) == 0
+            ), "Проверьте название таблицы"
+            assert (
+                df.shape[0] == df_check.shape[0]
+            ), "Проверьте размер таблицы, получаемый в ходе выполнения скрипта"
+            assert df_check.equals(df), "Проверьте, что скрипт написан согласно заданию"
+            st.success("Все верно! Ключ = 131")
+        except Exception as ex:
+            if ("Проверьте" in str(ex)) or ("не предусмотрено" in str(ex)):
+                st.error(ex)
+            else:
+                st.error(
+                    f"Скрипт написан некорретно (неполностью, либо вовсе отсутствует). Error message: {ex}"
+                )
+except Exception as ex:
+            if ("Проверьте" in str(ex)) or ("не предусмотрено" in str(ex)):
+                st.error(ex)
+            else:
+                st.error(
+                    f"Скрипт написан некорретно (неполностью, либо вовсе отсутствует). Error message: {ex}"
+                )
